2acd37c54579b3476603cf523857f0c59da23053
Rodrigues Filho <ednofco@gmail.com>
2015-07-09 09:22:29 -0300

    Comentarios adicionados no fonte

--- pratica5.asm
+++ pratica5.asm
@@ -1,1 +1,25 @@
-; Pratica 5
+; Pratica 5 : Interface de teclado matricial
+; Programador: Francisco Edno
+;
+; O programa continuamente faz a leitura de um teclado matricial
+; na porta P2 e imprime em um display de 7 segmentos. Esse codigo 
+; nao faz uso de interrupcoes e eh puramente didatico.
+;
+; A interface com o teclado funciona da seguinte maneira:
+; Aciono cada coluna sequencialmente com zero e faco, leitura da porta p2
+; e decodifico com uma tabela.
+;
+; Teclado matricial:
+; C3: P2.0 | C2: P2.1 | C1: P2.2
+; L4: P2.3 | L3: P2.4 | L2: P2.5 | L1: P2.6
+;
+; Registrador de deslocamento:
+; SHD: P0.0 | SHCK: P0.1 | SHLTCH: P0.2
+
+; ****************************************************************************
+; EQUATES
+; ****************************************************************************
+
+SHD		equ		p0.0
+SHCK	equ		p0.1
+SHLTCH	equ		p0.2
